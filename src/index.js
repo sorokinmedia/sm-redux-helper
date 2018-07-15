4ce12ce76ff7a486d11c213ab4532541215bc1f5
varenik163 <varenik163@yandex.ru>
2018-07-15 12:51:59 +0400

    first commit

---
+++ src/index.js
@@ -0,0 +1,39 @@
+import {buildUrlSearch} from "sm-string-helper";
+export const
+	URL = 'URL',
+	FAIL = 'FAIL',
+	SUCCESS = 'SUCCESS',
+	START = 'START';
+
+export const bypassReducer = (baseType, savePrevData = true) => {
+	return (data = {}, action) => {
+		const { type, response, error } = action;
+		const prevData = savePrevData && data;
+
+		switch(type) {
+			case baseType + START:
+				return { ...prevData, loading: true };
+			case baseType + SUCCESS:
+				return response;
+			case baseType + FAIL:
+				return {
+					...prevData,
+					error
+			};
+		}
+
+		return data;
+	};
+};
+
+export const bypassAction = (type, url, callType, payload, params, auth = true) => {
+	return () => {
+		return {
+			type,
+			[callType]: URL + url + buildUrlSearch(params),
+			need_auth_token: auth,
+			payload,
+			queryData: payload
+		};
+	};
+};
